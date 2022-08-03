1e074a48d0cb25c89628041eee204a4c3011ca8c
JustYourBud <59189197+JustYourBud@users.noreply.github.com>
2022-08-03 00:35:13 -0500

    ✨ Add /balance command
    Close #3

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,6 +1,9 @@
 import { CommandList } from "../classes/CommandList";
+import { balance } from "./economy/balance";
 
 /**
  * The list of commands to be registered when the Client starts.
  */
-export const commandList = new CommandList({});
+export const commandList = new CommandList({
+	"balance": balance,
+});
