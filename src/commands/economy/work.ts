35d68c124c43304f61284bcce89cddbbb9086b05
JustYourBud <59189197+JustYourBud@users.noreply.github.com>
2022-08-05 00:01:27 -0500

    ✨ Add random jobs and descriptions to /work command

--- src/commands/economy/work.ts
+++ src/commands/economy/work.ts
@@ -8,6 +8,7 @@ import {
 	SlashCommandBuilder,
 } from "discord.js";
 import { Command } from "../../classes/Command";
+import { Job } from "../../classes/economy/Job";
 
 /** The data of the command, including subcommands and options if applicable. */
 const data = new SlashCommandBuilder()
@@ -18,24 +19,29 @@ const data = new SlashCommandBuilder()
 const run = async (interaction: CommandInteraction) => {
 	await interaction.deferReply();
 
+	// Creates three randomly generated job titles
+	const jobOne = new Job();
+	const jobTwo = new Job();
+	const jobThree = new Job();
+
 	// Embed displaying the job choices
 	const workEmbed = new EmbedBuilder()
 		.setTitle("Work")
 		.setDescription("Please select from one of the following jobs.")
-		.setThumbnail("https://cdn.discordapp.com/emojis/684043360624705606.png")
+		.setThumbnail("https://cdn.discordapp.com/emojis/684043360624705606")
 		.setColor(0xffc27e)
 		.addFields(
 			{
-				"name": "<:option1:785555664856547378> `Job 1`",
-				"value": "Pay: ~<:raycoin:684043360624705606>500\nDescription",
+				"name": `<:option1:785555664856547378> \`${ jobOne.title }\``,
+				"value": `Pay: ~<:raycoin:684043360624705606>500\n${ jobOne.description }`,
 			},
 			{
-				"name": "<:option2:785555675144257536> `Job 2`",
-				"value": "Pay: ~<:raycoin:684043360624705606>1000\nDescription",
+				"name": `<:option2:785555675144257536> \`${ jobTwo.title }\``,
+				"value": `Pay: ~<:raycoin:684043360624705606>1000\n${ jobTwo.description }`,
 			},
 			{
-				"name": "<:option3:785555684799938630> `Job 3`",
-				"value": "Pay: ~<:raycoin:684043360624705606>1500\nDescription",
+				"name": `<:option3:785555684799938630> \`${ jobThree.title }\``,
+				"value": `Pay: ~<:raycoin:684043360624705606>1500\n${ jobThree.description }`,
 			},
 		);
 
@@ -43,22 +49,22 @@ const run = async (interaction: CommandInteraction) => {
 	const workSelectMenuRow =
 		new ActionRowBuilder<SelectMenuBuilder>().addComponents(new SelectMenuBuilder()
 			.setCustomId("workSelectMenu")
-			.setPlaceholder("Select job")
+			.setPlaceholder("Select a job")
 			.addOptions([
 				{
-					"label": "Job 1",
+					"label": jobOne.title,
 					"description": "Cooldown: 2 hours",
 					"emoji": "785555664856547378",
 					"value": "jobOne",
 				},
 				{
-					"label": "Job 2",
+					"label": jobTwo.title,
 					"description": "Cooldown: 5 hours",
 					"emoji": "785555675144257536",
 					"value": "jobTwo",
 				},
 				{
-					"label": "Job 3",
+					"label": jobThree.title,
 					"description": "Cooldown: 8 hours",
 					"emoji": "785555684799938630",
 					"value": "jobThree",
@@ -67,7 +73,7 @@ const run = async (interaction: CommandInteraction) => {
 
 	// Action row containing the buttons
 	const workButtonRow = new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder()
-		.setCustomId("cancel")
+		.setCustomId("cancelButton")
 		.setLabel("Cancel")
 		.setStyle(ButtonStyle.Danger)
 		.setEmoji("785336733696262154"));
