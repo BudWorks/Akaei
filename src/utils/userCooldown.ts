97fecf5210c510ca73eeeda752ee4b8c47ea48de
JustYourBud <59189197+JustYourBud@users.noreply.github.com>
2022-08-20 01:50:52 -0500

    ✨ Add check if user exists when getting cooldown data

--- src/utils/userCooldown.ts
+++ src/utils/userCooldown.ts
@@ -3,10 +3,24 @@ import { User } from "../database/models/User";
 /**
  * Finds and returns the specified cooldown data of the specified user.
  * @param id The Snowflake ID of the user.
- * @param cooldownType The field to get the cooldown from, depending on the command.
+ * @param username The Discord username of the user.
  * @returns Returns an object of the user's specified cooldown data.
  */
-export async function getCooldown (id: string) {
+export async function getCooldown (id: string, username: string) {
 	/** The cooldown data of the specified User. */
-	return await User.findOne({ "_id": id }, "cooldowns");
+	let user = await User.findOne({ "_id": id }, "cooldowns");
+
+	// Checks if the specified User has cooldown data or not
+	if (!user) {
+		// Creates a document for the User if they cannot be found
+		user = await User.create({
+			"_id": id,
+			"username": username,
+		});
+		return user;
+	}
+	else {
+		// Returns the User's cooldown data
+		return user;
+	}
 }
